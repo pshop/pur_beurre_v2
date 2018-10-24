43dfb003e738c460a5adb38a950010bd9a334c71
pshop <choppin.p@gmail.com>
2018-10-24 18:27:21 +0200

    heroku config

--- pur_beurre_v2/settings.py
+++ pur_beurre_v2/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+
 from . import my_vars
+
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -137,3 +140,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+django_heroku.settings(locals())
