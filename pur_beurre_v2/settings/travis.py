5ee6b243d5aa069aba88cabe49376299ca5c6b53
pshop <choppin.p@gmail.com>
2018-11-06 14:01:37 +0100

    settings travis user

--- pur_beurre_v2/settings/travis.py
+++ pur_beurre_v2/settings/travis.py
@@ -4,7 +4,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': '',
-        'USER': '',
+        'USER': 'postgres',
         'PASSWORD': '',
         'HOST': '',
         'PORT': '',
