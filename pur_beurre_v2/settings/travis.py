fcbac345117773f21ccd21dbaad05969c12b5a4f
pshop <choppin.p@gmail.com>
2018-12-11 13:06:55 +0100

    oups

--- pur_beurre_v2/settings/travis.py
+++ pur_beurre_v2/settings/travis.py
@@ -11,17 +11,3 @@ DATABASES = {
     },
 }
 
-from . import *
-
-import sentry_sdk
-from sentry_sdk.integrations.django import DjangoIntegration
-
-sentry_sdk.init(
-    dsn="https://d767892c56054439a3eb13cc3824706b@sentry.io/1317774",
-    integrations=[DjangoIntegration()]
-)
-
-SECRET_KEY = ')kto17qw(m#8)g0bc)9#v3$p5rq(v@$u0zlezr7**73&ixp35v'
-DEBUG = False
-ALLOWED_HOSTS = ['pshop.me', 'www.pshop.me', '46.101.170.37']
-
